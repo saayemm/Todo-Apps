ef9e535f7118f2603ee770b239544a4555006d5c
Saayemm <sayemdcc@gmail.com>
2024-12-10 18:48:23 +0600

    component-create

--- Todo-app/src/App.jsx
+++ Todo-app/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import Header from './components/Header'
+import Tabs from './components/Tabs'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
 
 function App() {
 
 
   return (
     <>
-      
+      <Header/>
+      <Tabs/>
+      <TodoList/>
+      <TodoInput/>
     </>
   )
 }
